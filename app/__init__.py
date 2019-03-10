6bb43b3d4ad2b4e98647a63e9a8bd2b8b9481fd7
Neko Reid <neko.reid@mymona.uwi.edu>
2019-03-10 14:59:04 +0000

    trying to fix heroku

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'Sup3r$3cretkey'
 UPLOAD_FOLDER = './app/static/pro-pics'
 
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/profiles"
+app.config['SQLALCHEMY_DATABASE_URI'] = "'postgresql://lpgaueufxtupxk:c827a24e8428cdc85ab6d2f18d487cee9e7a85b36eec095bd467b589016e79d3@ec2-54-221-236-144.compute-1.amazonaws.com:5432/ddvirpdt68llo5'"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
