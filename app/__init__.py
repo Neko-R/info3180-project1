0605522e7b76b2fb1c1c916842ad12632c582dfd
Neko Reid <neko.reid@mymona.uwi.edu>
2019-03-10 15:11:19 +0000

    trying

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'Sup3r$3cretkey'
 UPLOAD_FOLDER = './app/static/pro-pics'
 
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "'postgresql://lpgaueufxtupxk:c827a24e8428cdc85ab6d2f18d487cee9e7a85b36eec095bd467b589016e79d3@ec2-54-221-236-144.compute-1.amazonaws.com:5432/ddvirpdt68llo5'"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://klpvgqzuvfyvlm:50d24867a9ecebafbdbf28f26b6940cc88b0f8b2eed7734006aa1e6e5a556447@ec2-54-197-232-203.compute-1.amazonaws.com:5432/dfkfpm70be1knh"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
